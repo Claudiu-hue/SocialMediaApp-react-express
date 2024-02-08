45ae6ae39362a40e91eb93ce7e31c5be1d7159bd
Claudiu-hue <claudiuluca07@gmail.com>
2024-02-08 14:10:48 +0200

    .

---
+++ api/connect.js
@@ -0,0 +1,9 @@
+import mysql2 from "mysql2";
+import dotenv from "dotenv/config";
+
+export const db = mysql2.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
